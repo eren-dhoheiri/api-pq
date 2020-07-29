bbc99f84fb7840f8cc1d84cb054788e398b03da2
eren-dhoheiri <dhoheiri@gmail.com>
2020-07-29 13:09:54 +0700

    set  ain

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/eren-dhoheiri/api-pq
 
 go 1.14
+
+require github.com/lib/pq v1.8.0
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/eren-dhoheiri/api-pq/config"
+)
 
 func main() {
-	fmt.Println("Helllo")
+	fmt.Println("Hello")
+
+	db, err := config.GetPostgresDB()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 }
